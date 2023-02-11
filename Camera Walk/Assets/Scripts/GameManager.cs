581d34f664dc5b4d412e3c678466635c5b7429bf
lawsuit3310 <72363043+lawsuit3310@users.noreply.github.com>
2023-02-11 15:37:02 +0900

    23
    234

---
+++ Camera Walk/Assets/Scripts/GameManager.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] camControllers = new GameObject[2];
+    public GameObject[] backGrounds = new GameObject[2];
+
+    //true : gotoLeft, false : gotoRight;
+    private bool Direction = true;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        #region It Works in Game Scene
+        if (SceneManager.GetActiveScene().name == "Game")
+        {
+            Debug.Log("Game Scene");
+            int i = 0;
+            foreach (GameObject CameraController in GameObject.FindGameObjectsWithTag("CameraController"))
+            {
+                //camControllers[0] == left
+                //camControllers[1] == right
+                Debug.Log(CameraController.name);
+                camControllers[i] = CameraController;
+                i++;
+            }
+
+            i = 0;
+
+            foreach (GameObject BackGround in GameObject.FindGameObjectsWithTag("BackGround"))
+            {
+                //backGrounds[0] == Beach
+                //backGrounds[1] == Woods
+                Debug.Log(BackGround.name);
+                backGrounds[i] = BackGround;
+                i++;
+            }
+
+            camControllers[0].gameObject.SetActive(false);
+        }
+        #endregion
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    #region method belongs UI
+    public void GotoLeft()
+    {
+        try
+        {
+            Direction = true;
+            camControllers[0].gameObject.SetActive(false);
+            camControllers[1].gameObject.SetActive(true);
+            StartCoroutine("BackGroundFadeEffect");
+        }
+        catch
+        {
+            Debug.LogError("Unexpected. (1)");
+        }
+    }
+    public void GotoRight()
+    {
+        try
+        {
+            Direction = false;
+            camControllers[0].gameObject.SetActive(true);
+            camControllers[1].gameObject.SetActive(false);
+            StartCoroutine("BackGroundFadeEffect");
+
+        }
+        catch
+        {
+            Debug.LogError("Unexpected. (2)");
+        }
+    }
+    #endregion
+
+    #region Coroutines
+    IEnumerator BackGroundFadeEffect() {
+
+        for(int i = 0 ;i < 20 ; i++)
+        {
+            foreach (GameObject backGround in backGrounds)
+            {
+                Vector3 pos = backGround.transform.position;
+                backGround.transform.position = new Vector3(
+                    Direction ? pos.x + 1 : pos.x - 1 , pos.y, pos.z);
+            }
+
+            yield return new WaitForSeconds(0.01f);
+        }
+        yield break;
+    }
+    #endregion
+}
---
+++ Camera Walk/Assets/Scripts/RayCast.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        //On Mouse click
+        if(Input.GetMouseButton(0))
+        {
+
+        }
+    }
+}
